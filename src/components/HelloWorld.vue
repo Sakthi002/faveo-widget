c6c601976f6d0a223f45abb3ce4404152d7a3baa
Sakthi002 <sakthivel.munusamy@ladybirdweb.com>
2023-07-28 12:49:25 +0530

    passing api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,12 +11,22 @@ import axios from 'axios';
 
     name : 'hello-world',
 
+    data () {
+
+      return {
+
+        baseUrl : ''
+      }
+    },
+
     beforeMount() {
 
       // Get the value of the 'params' parameter from the iframe URL
       var urlParams = new URLSearchParams(window.location.search);
       var paramsValue = urlParams.get('params');
-      console.log(paramsValue),'retrived value'; // This will log 'test' in the iframe's console
+      console.log(paramsValue,'retrived'); // This will log 'test' in the iframe's console
+
+      this.baseUrl = paramsValue;
 
       this.getArticles()
     },
@@ -25,7 +35,7 @@ import axios from 'axios';
 
       getArticles() {
 
-        axios.get('https://support.faveohelpdesk.com/api/article-list-with-category').then(res=>{
+        axios.get(this.baseUrl+'/api/article-list-with-category').then(res=>{
           console.log(res.data,'dddtest')
         }).catch(err=>{
           console.error(err)
